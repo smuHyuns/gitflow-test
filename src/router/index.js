03e140b3939dea05b217245eb5e5218c47eae621
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-06-10 15:10:12 +0900

    20240610 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../pages/Home.vue';
+import Home from '../pages/Home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
